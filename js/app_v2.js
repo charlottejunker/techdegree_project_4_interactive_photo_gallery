99a3e93d1d9d110445a8c561c993360696a4ad6c
Charlotte Junker <cejunker@gmail.com>
2017-01-08 15:07:14 +0000

    fixed reload issue by attaching and detaching iframe

--- js/app_v2.js
+++ js/app_v2.js
@@ -1,43 +1,42 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $lightbox = $('<div id="lightbox"></div>');
-var $image = $('<img id="selectedImg">');
-var $iframe = $('<iframe id="selectedVid"></iframe>');
+var $content = $('<img id="selectedImg"><iframe id="selectedVid"></iframe>');
 var $caption = $('<p></p>');
 var $next = $('<button id="next"><img src="img/next.png"></button>');
 var $prev = $('<button id="prev"><img src="img/prev.png"></button>');
 
 //add img or vid to lightbox div + add caption to overlay
-$lightbox.append($prev).append($image).append($iframe).append($next).append($caption);;
+$lightbox.append($prev).append($content).append($next).append($caption);;
 //add lightbox div to overlay
 $overlay.append($lightbox);
 //add overlay
 $("body").append($overlay);
 
 //array of image objects for imgs and vids inside .gallery
-var contentGallery = [];
+var $contentGallery = [];
 
 //array of href attributes of the links inside .gallery (note to me: .map = Pass each element in the current matched set through a function, producing a new jQuery object containing the return values.)
-var contentSrcArray = $(".content").map(function(){
+var $contentSrcArray = $(".content").map(function(){
   return $(this).attr("href");
 });
 
 //array of title attributes of images
-var contentCaptionArray = $(".content img").map(function(){
+var $contentCaptionArray = $(".content img").map(function(){
   return $(this).attr("alt");
 });
 
-//array of title attributes of images
-var contentCaptionArray = $(".content img").map(function(){
-  return $(this).attr("alt");
+var $type = $(".content").map(function(){
+  return $(this).attr("class");
 });
 
-//populate contentGallery
-for (var i = 0; i < contentSrcArray.length; i++) {
-  contentGallery.push({
-    src: contentSrcArray[i],
-    title: contentCaptionArray[i],
-    location: i
+//populate $contentGallery
+for (var i = 0; i < $contentSrcArray.length; i++) {
+  $contentGallery.push({
+    src: $contentSrcArray[i],
+    title: $contentCaptionArray[i],
+    location: i,
+    type: $type[i],
   });
 }
 
@@ -51,38 +50,29 @@ function findCurrentContent(arrayOfObjects, src) {
   }
 }
 
+
 // function that animates the image on the transition
 function animateImage() {
-  $image.hide();
-  $image.fadeIn("fast");
+  $content.hide();
+  $content.fadeIn("fast");
 }
 
 //Capture click event on link to the image
-$(".img").click(function(event){
+$(".content").click(function(event){
   event.preventDefault();
   var contentLocation = $(this).attr("href");
   //add img to overlay
-  $image.attr("src", contentLocation);
+  $("#selectedImg").attr("src", contentLocation);
   //get child's alt attr and add caption 600px wide in desktop
   var captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
-  //detach appended iframe
-  $iframe.detach();
-  //show overlay to main site - medium opacity
-  $overlay.show();
-});
-
-//Capture click event on link to the image
-$(".vid").click(function(event){
-  event.preventDefault();
-  var contentLocation = $(this).attr("href");
-  //add img to overlay
-  $iframe.attr("src", contentLocation);
-  //get child's alt attr and add caption 600px wide in desktop
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
-  //detach appended img
-  $image.detach();
+  if ($(this).hasClass("vid")) {
+    //add video to overlay
+    $("#selectedVid").attr("src", contentLocation);
+    $("#selectedImg").hide();
+  } else {
+    $("#selectedVid").hide();
+  }
   //show overlay to main site - medium opacity
   $overlay.show();
 });
@@ -91,15 +81,37 @@ function next() {
   //get current src attr from img
   var contentCurrentSrc = $prev.next().attr("src");
   //get all attrs for current img
-  var currentContent = findCurrentContent(contentGallery, contentCurrentSrc);
+  var currentContent = findCurrentContent($contentGallery, contentCurrentSrc);
   //to cycle from last img to 1st
-  if (currentContent.location === contentGallery.length - 1) {
-    $prev.next().attr("src", contentGallery[0].src);
-    $caption.text(contentGallery[0].title);
+  if (currentContent.location === $contentGallery.length - 1) {
+    $prev.next().attr("src", $contentGallery[0].src);
+    $caption.text($contentGallery[0].title);
+    //access the type of the 1st in the array
+    var firstContent = $contentGallery[0].type;
+    if (firstContent === "content img") {
+      $("#selectedVid").hide();
+      $("#selectedImg").show();
+    } else {
+      $prev.siblings("iframe").attr("src", $contentGallery[0].src);
+      $("#selectedVid").show();
+      $("#selectedImg").hide();
+    }
   } else {
     //to cycle to next img
-    $prev.next().attr("src", contentGallery[currentContent.location + 1].src);
-    $caption.text(contentGallery[currentContent.location + 1].title);
+    $prev.next().attr("src", $contentGallery[currentContent.location + 1].src);
+    $caption.text($contentGallery[currentContent.location + 1].title);
+    //access the next type in the array
+    var nextContent = $contentGallery[currentContent.location + 1].type;
+    //check the type of the next img/vid and show/hide iframe/img as necessary
+    if (nextContent === "content vid") {
+      $("#selectedImg").hide();
+      $("#selectedVid").show();
+      //if not moving from end to 1st
+      $prev.siblings("iframe").attr("src", $contentGallery[currentContent.location + 1].src);
+    } else {
+      $("#selectedImg").show();
+      $("#selectedVid").hide();
+    }
   }
 }
 
@@ -107,19 +119,39 @@ function prev() {
   //get current src attr from img
   var contentCurrentSrc = $prev.next().attr("src");
   //get all attrs for current img
-  var currentContent = findCurrentContent(contentGallery, contentCurrentSrc);
+  var currentContent = findCurrentContent($contentGallery, contentCurrentSrc);
+
   //to cycle from 1st img to last
   if (currentContent.location === 0) {
-    $prev.next().attr("src", contentGallery[contentGallery.length - 1].src);
-    $caption.text(contentGallery[contentGallery.length - 1].title);
+    $prev.next().attr("src", $contentGallery[$contentGallery.length - 1].src);
+    $caption.text($contentGallery[$contentGallery.length - 1].title);
+    //access the type of the last in the array
+    var lastContent = $contentGallery[$contentGallery.length - 1].type;
+    if (lastContent === "content img") {
+      $("#selectedVid").hide();
+      $("#selectedImg").show();
+    } else {
+      $prev.siblings("iframe").attr("src", $contentGallery[$contentGallery.length - 1].src);
+      $("#selectedVid").show();
+      $("#selectedImg").hide();
+    }
   } else {
     //to cycle to prev img
-    $prev.next().attr("src", contentGallery[currentContent.location - 1].src);
-    $caption.text(contentGallery[currentContent.location - 1].title);
+    $prev.next().attr("src", $contentGallery[currentContent.location - 1].src);
+    $caption.text($contentGallery[currentContent.location - 1].title);
+    var prevContent = $contentGallery[currentContent.location - 1].type;
+    //check the type of the prev img/vid and show/hide iframe/img as necessary
+    if (prevContent === "content vid") {
+      $("#selectedImg").hide();
+      $("#selectedVid").show();
+      //if not moving from 1st to end
+      $prev.siblings("iframe").attr("src", $contentGallery[currentContent.location - 1].src);
+    } else {
+      $("#selectedImg").show();
+      $("#selectedVid").hide();
+    }
   }
 }
-
-
 
 //when next button clicked
 //capture click event
@@ -155,17 +187,20 @@ $(document).ready().keydown(function( event ) {
 $lightbox.click(function(e){
    if(e.target != this) return; // only continue if the target itself has been clicked
    $overlay.hide();
+   location.reload();
 });
 //lightbox clicked
 $overlay.click(function(e){
    if(e.target != this) return; // only continue if the target itself has been clicked
    $overlay.hide();
+   location.reload();
 });
 
 //esc button pressed
 $(document).ready().keydown(function( event ) {
   if ( event.which == 27 ) {
     $overlay.hide();
+    location.reload();
   }
 });
 
@@ -184,7 +219,6 @@ $("#search").keyup(function(){
     //match search term to alt attr
     $(".content img").each(function(){
       var currentKeyword = $(this).attr("alt").toLowerCase();
-      console.log(currentKeyword);
       if (currentKeyword.indexOf(currentQuery) >= 0) {
         //show images that do match
         $(this).parent().show();
@@ -196,3 +230,10 @@ $("#search").keyup(function(){
   }
 
 });
+
+//get width of iframe
+var bodyWidthPx = $("body").width();
+var iframeWidth = bodyWidthPx * 0.8;
+
+//set height to 56.25% of height
+$("#selectedVid").height(iframeWidth * 0.5625);
